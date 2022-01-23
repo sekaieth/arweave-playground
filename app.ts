42831775b1a3a72674a307ac31b7cfe21a3767f9
Brian Cable <brian.cable.dev@gmail.com>
2022-01-23 00:17:10 -0600

    arlocal support for uploading images and getting data

--- app.ts
+++ app.ts
@@ -1,73 +1,124 @@
-import * as key from "./arweave-key-lvNDkdPnmY5e26mS2SqYW-u5B89ixGywoqT7c5vgaB8.json";
+//import * as key from "./arweave-key-lvNDkdPnmY5e26mS2SqYW-u5B89ixGywoqT7c5vgaB8.json";
 import InitArweave from "./arweave_components/initialize-arweave";
 import * as fs from 'fs';
 
+// instead of continuing to fight node-fetch, i came across this alternative that can just be imported
+import fetch from 'cross-fetch';
 
 const app = async () => {
 
     const delay = ms => new Promise(res => setTimeout(res, ms));
-
-    // Get Wallet Address
-    const wallet = await InitArweave.wallets.jwkToAddress(key).then((address) => {
-        console.log("wallet address:", address);
-        return(address);
-    });
-
-
-    // Get Wallet Balance
-    const walletBalance = await InitArweave.wallets.getBalance(wallet);
-    console.log("Wallet balance:", InitArweave.ar.winstonToAr(walletBalance), "AR");
-
-
-    // Send a file to Arweave storage - READ HERE: https://github.com/ArweaveTeam/arweave-js#submit-a-transaction
-    // let data = fs.readFileSync("./storage/4.png");
-
-    // const tx = await InitArweave.createTransaction({ data: data }, key);
-    // tx.addTag("Content-Type", 'images/math-doge');
-
-    // await InitArweave.transactions.sign(tx, key);
-
-    // let uploader = await InitArweave.transactions.getUploader(tx);
-
-    // while (!uploader.isComplete) {
-    //     await uploader.uploadChunk();
-    //     console.log(`${uploader.pctComplete}% complete, ${uploader.uploadedChunks}/${uploader.totalChunks}`);
-    // }
-
-    // console.log(tx.id);
-
-
     
-    // const response = await InitArweave.transactions.getStatus(tx.id).then(res => {
-    //     return(res);
-    // })
+    // generate wallet
+    const generatedKey = await InitArweave.wallets.generate();
 
-    // while (!response.confirmed) {
-    //     delay(20000)
-    //     await InitArweave.transactions.getStatus(tx.id).then(res => {
-    //         return(res) 
-    //     })
-    // }
+    // get address from the wallet
+    const walletAddress = await InitArweave.wallets.jwkToAddress(generatedKey);
 
-    // console.log(response);
-
-
-    let tx = "1dt4OLT3Kx_f7J3AwqmXbNseU8uK31zcj_XUbmYyuLI"
-
-    // Get Transaction data
-    async function getTransactionData(_tx) {
-        await InitArweave.transactions.getData(_tx).then(data => {
-            const decode = (data: string):string => Buffer.from(data, 'base64').toString('binary');
-            console.log(decode)
-        })
+    // mint 1 AR to that wallet address
+    const res = await fetch(`http://localhost:1984/mint/${walletAddress}/1000000000000`);
+    if (res.status >= 400) {
+        throw new Error("Bad response from server");
     }
 
-    getTransactionData(tx);
-
+    // get and show wallet address
+    const walletBalance = await InitArweave.wallets.getBalance(walletAddress);
+    console.log("Wallet balance:", InitArweave.ar.winstonToAr(walletBalance), "AR");
     
+    // Send a file to Arweave storage - READ HERE: https://github.com/ArweaveTeam/arweave-js#submit-a-transaction
+    let data = fs.readFileSync("./storage/4.png");
+
+    // create transaction
+    const tx = await InitArweave.createTransaction({ data: data }, generatedKey);
+    tx.addTag("Content-Type", 'images/math-doge');
+
+    // sign transaction
+    await InitArweave.transactions.sign(tx, generatedKey);
+
+    // upload file
+    let uploader = await InitArweave.transactions.getUploader(tx);
+    while (!uploader.isComplete) {
+         await uploader.uploadChunk();
+         console.log(`${uploader.pctComplete}% complete, ${uploader.uploadedChunks}/${uploader.totalChunks}`);
+    }
+
+    console.log(tx.id);
+
+    // get status until success or failure.... NOTE: while testing, the confirmed property will just log as saying 'Pending'
+    // until you go to http://localhost:1894/mine to mine a block, which will put it on the local chain
+    var success = false;
+    var loops = 0;
+
+    while (!success) {
+
+        await delay(700)
+
+        // after 3 loops, i'm throwing a call to 'mine' in here, just to simulate waiting for a block to be mined to verify the transaction
+        if(loops == 3)
+        {
+            fetch('http://localhost:1984/mine');
+        }
+
+        let response = await InitArweave.transactions.getStatus(tx.id);
+
+        console.log(response);
+
+        // if there's an error, break out of the loop
+        if(response.status >= 400)
+        {
+            break;
+        }
+        else if(response.status === 200 && response.confirmed?.number_of_confirmations >= 0)
+        {
+            success = true;
+        }
+
+        loops += 1;
+    }
+
+    console.log(`Upload confirmed: ${success}`);
+
+    if(success)
+    {
+        let decodedData = await getTransactionData2(tx.id);
+        console.log(`\n\n--------------------------------------------\nDecoded data: ${decodedData.substring(0, 500)}...`);
+    }
+
  }
 
 
+// Get Transaction data
+// NOTE: i'm getting errors that it's unable to get the chunk at offset -143884. so i tried a different
+// function with calling what's in the arlocal api, and determined you can get the offset from tx/id/offset
+// and then the chunk of data from chunk/offset (as a positive number, not negative)
+// ...so I'm not sure why this passing in a negative here. anyway the other function is bringing back raw data
+async function getTransactionData(txId) {
+    console.log(`Transaction Id: ${txId}`);
+    await InitArweave.transactions.getData(txId).then(data => {
+        console.log(data);
+        // CjwhRE9DVFlQRSBodG1sPgo...
+    });
+}
+
+async function getTransactionData2(txId) {
+    console.log(`Transaction Id: ${txId}`);
+    const offset = await fetch(`http://localhost:1984/tx/${txId}/offset`).then((res:any) => {
+        return res.json();
+    }).then((data) => {
+        return(data.offset);
+    });    
+    console.log(`Offset: ${offset}`);
+
+    const chunk:string = await fetch(`http://localhost:1984/chunk/${offset}`).then((res:any) => {
+        return res.json();
+    }).then((data) => {
+        return(data.chunk);
+    });
+    console.log(`chunk: ${chunk.substring(0,500)}...`);
+
+    const decode = Buffer.from(chunk, 'base64').toString('binary');
+    return decode;
+}
 
 
 app();
